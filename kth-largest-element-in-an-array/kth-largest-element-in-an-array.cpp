1072faa9949ceb3ce9ced2b3607f2c8907810912
emli <emilbeksulaymanov@gmail.com>
2021-06-15 01:40:16 +0600

    Time: 44 ms (15.14%), Space: 10 MB (83.08%) - LeetHub

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(true){
+            int pos = partition(nums,left,right);
+
+            unsigned long x = nums.size() - k;
+            if (pos < x){
+                left = pos + 1;
+            }else if (pos > x){
+                right = pos - 1;
+            }else {
+                return nums[pos];
+            }
+        }
+
+        return -1;
+    }
+
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left];
+
+        int cur = left + 1;
+
+        for(int i = left + 1; i <= right; i++){
+            if (nums[i] <= pivot){
+                swap(nums[i],nums[cur++]);
+            }
+        }
+
+        swap(nums[cur - 1], nums[left]);
+
+        return cur - 1;
+    }
+};
