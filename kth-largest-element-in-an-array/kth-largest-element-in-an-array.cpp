65fd1f611318bb7b14eaec89ece788fbdee4cc23
emli <emilbeksulaymanov@gmail.com>
2021-06-15 22:42:40 +0600

    Time: 260 ms (5.08%), Space: 9.9 MB (97.32%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    // test
+    int t = 0;
     int findKthLargest(vector<int>& nums, int k) {
         int left = 0;
         int right = nums.size() - 1;
-
+    
         while(true){
             int pos = partition(nums,left,right);
-
+            t++;
             unsigned long x = nums.size() - k;
             if (pos < x){
                 left = pos + 1;
             }else if (pos > x){
                 right = pos - 1;
             }else {
+                cout <<"Количество операций = "<< t << endl;
                 return nums[pos];
             }
         }
-
+        
         return -1;
     }
-
+     
     int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[left];
-
-        int cur = left + 1;
-
-        for(int i = left + 1; i <= right; i++){
-            if (nums[i] <= pivot){
-                swap(nums[i],nums[cur++]);
+        int pivot = left;
+    
+        while(left <= right){
+            if (nums[left] > nums[pivot] && nums[right] < nums[pivot]){
+                swap(nums[left++],nums[right--]);
+            }
+            if (nums[left] <= nums[pivot]){
+                left++;
+            }
+            if (nums[right] >= nums[pivot]){
+                right--;
             }
         }
-
-        swap(nums[cur - 1], nums[left]);
-
-        return cur - 1;
+        swap(nums[left - 1], nums[pivot]);
+        return left - 1;
     }
 };
