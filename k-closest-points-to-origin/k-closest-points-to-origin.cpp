83f0a9173931647974eb715ffc0beb43f3c8a145
emli <emilbeksulaymanov@gmail.com>
2021-06-16 14:39:45 +0600

    Time: 140 ms (90.34%), Space: 49.2 MB (90.13%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     vector<vector<int>> kClosest_withPartial_sort(vector<vector<int>>& points, int k) {
         partial_sort(points.begin(),points.begin() + k, points.end(), [](vector<int> &p,vector<int> &q){
             return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
