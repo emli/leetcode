621e6abfd8e42455f03104edb5981e822bb29535
emli <emilbeksulaymanov@gmail.com>
2021-08-19 10:04:20 +0600

    Time: 60 ms (73.90%), Space: 15.4 MB (20.92%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pos = {}
+        
+        size = len(nums)
+        for i in range(0,size):
+            complement = target - nums[i]
+            if complement in pos:
+                return [i,pos[complement]]
+            pos[nums[i]] = i
+        
+        
